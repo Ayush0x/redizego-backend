cd4fad4c97a1a6cf75117b91bce0b51b671f70bf
Ayush Sharma <ayush251299@gmail.com>
2025-07-14 19:54:43 +0530

    feat: integrate email service using Spring Mail
    - Added spring-boot-starter-mail dependency and SMTP configuration
    - Created EmailService using SimpleMailMessage
    - Verified sending via Spring Boot test
    - Endpoint not exposed yet; will be connected during workflow testing

--- src/test/java/com/redizego/redi_ze_go/RediZeGoApplicationTests.java
+++ src/test/java/com/redizego/redi_ze_go/RediZeGoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.redizego.redi_ze_go;
 
+import com.redizego.redi_ze_go.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RediZeGoApplicationTests {
 
+
+	@Autowired
+	private EmailSenderService emailSenderService;
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail("ayush251299@gmail.com","this is test email","this is the body of test email");
 	}
 
+	@Test
+	void sendEmailsMultiple(){
+		String[] emails={"ayush251299@gmail.com",
+				"tf141st@gmail.com",
+				"scriptsensei593@gmail.com",
+		"rohit.verma.rv.4682@gmail.com"};
+
+		emailSenderService.sendEmail(emails,
+				"This is a test mail",
+				"This is the body of the email");
+	}
 }
